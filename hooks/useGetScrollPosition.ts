a87e34e5bae0b3d3d69d9df804a44b0903847805
vigneshwaran <vigneshwaran.ravi@makoitlab.com>
2025-01-18 00:18:58 +0530

    enhance: added scroll animation using framer

--- hooks/useGetScrollPosition.ts
+++ hooks/useGetScrollPosition.ts
@@ -1,12 +1,38 @@
 import { useEffect, useState } from "react";
+import { debounce } from "lodash";
 
-const useGetScrollPosition = () => {
+// maxScroll: Adjust based on how much scroll you want to fully transition (optional)
+const useGetScrollPosition = ({
+  maxScroll = 0,
+  smooth = false,
+}: {
+  maxScroll?: number;
+  smooth?: boolean;
+}) => {
   const [scrollY, setScrollY] = useState(0);
 
   useEffect(() => {
-    const handleScroll = () => {
-      setScrollY(window.scrollY);
-    };
+    const handleScroll = smooth
+      ? () => {
+          console.log("🚀 ~ handleScroll ~ maxScroll:", maxScroll);
+          if (maxScroll > 0) {
+            const scrollY = window.scrollY;
+            const progress = Math.min(scrollY / maxScroll, 1); // Clamp progress to 1
+            setScrollY(progress);
+          } else {
+            setScrollY(window.scrollY);
+          }
+        }
+      : debounce(() => {
+          console.log("🚀 ~ handleScroll ~ maxScroll:", maxScroll);
+          if (maxScroll > 0) {
+            const scrollY = window.scrollY;
+            const progress = Math.min(scrollY / maxScroll, 1); // Clamp progress to 1
+            setScrollY(progress);
+          } else {
+            setScrollY(window.scrollY);
+          }
+        });
 
     // just trigger this so that the initial state
     // is updated as soon as the component is mounted
--- screens/landing/sections/Navbar.tsx
+++ screens/landing/sections/Navbar.tsx
@@ -1,21 +1,30 @@
 "use client";
-import { useGetScrollPosition } from "@/hooks";
 import React from "react";
 import { FaAngleDown } from "react-icons/fa6";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Navbar = () => {
-  const position = useGetScrollPosition();
+  const { scrollY } = useScroll();
+
+  // TODO: Need to make this smoother
+  const width = useTransform(scrollY, [0, 200], ["100vw", "90vw"]);
+  const borderRadius = useTransform(scrollY, [0, 200], ["0px", "40px"]);
+  const top = useTransform(scrollY, [0, 200], ["0px", "10px"]);
 
   return (
-    <header
-      className={`${
-        position > 150
-          ? "rounded-[30px] w-[92vw] m-auto top-4"
-          : "w-screen top-0"
-      } transition-all ease-in-out duration-300 h-[65px] backdrop-blur-xl drop-shadow-xl flex justify-between px-20 py-4 bg-white/40 rounded sticky `}
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      style={{
+        width,
+        borderRadius,
+        top,
+      }}
+      transition={{ duration: 0.5, ease: "easeInOut" }}
+      className={`header top-0 w-screen h-[65px] m-auto backdrop-blur-xl drop-shadow-xl flex justify-between px-20 py-4 bg-white/40 rounded sticky `}
     >
       <div>
-        <h3 className="font-bold text-2xl text-purplegreen">BOUC</h3>
+        <h3 className="font-bold text-2xl">BOUC</h3>
       </div>
       <ul className="flex gap-8">
         <Item toggle>Services</Item>
@@ -23,7 +32,7 @@ const Navbar = () => {
         <Item>Business Process</Item>
         <Item>Contact us</Item>
       </ul>
-    </header>
+    </motion.div>
   );
 };
 
